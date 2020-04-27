df4fd923164eab61926d29504573de6b09262569
Cesar GP <cgpcsjcmp@gmail.com>
2020-04-27 19:23:24 +0200

    fix: coverage 83 passing

--- index.js
+++ index.js
@@ -2,13 +2,9 @@ console.log("Deployed");
 
 module.exports.returnThousand = () => {
     console.log("returnThousand");
-    return 10000;
+    return 1000;
 }
 
 const returnHundred = () => {
-    console.log("returnHundred");
-    console.log("returnHundred");
-    console.log("returnHundred");
-    console.log("returnHundred");
     return 100;
 }
