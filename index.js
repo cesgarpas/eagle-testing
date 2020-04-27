51e52a7feb42e61c3b8f753c1e77adb0a275c891
Cesar GP <cgpcsjcmp@gmail.com>
2020-04-27 19:49:00 +0200

    fix: coverage and test

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ console.log("Deployed");
 
 module.exports.returnThousand = () => {
     console.log("returnThousand");
-    return 10000;
+    return 1000;
 }
 
 const returnHundred = () => {
-    console.log("returnThousand");
-    console.log("returnThousand");
-    console.log("returnThousand");
     return 100;
 }
