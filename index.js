ce65ea4228c4022b2b08dce35e582b97600630e6
Cesar GP <cgpcsjcmp@gmail.com>
2020-06-14 23:25:14 +0200

    test: increased a lot!

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ console.log("Deployed");
 
 module.exports.returnThousand = () => {
     console.log("returnThousand");
+    console.log("more lines!");
+    console.log("more lines!");
+    console.log("more lines!");
+    console.log("more lines!");
     return 1000;
 }
 
 const returnHundred = () => {
-    console.log("more lines!");
-    console.log("more lines!");
     console.log("more lines!");
     console.log("more lines!");
     return 100;
